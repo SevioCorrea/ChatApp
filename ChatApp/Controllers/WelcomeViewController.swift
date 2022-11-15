e9dc6b874c537e45c03ce0e2593ca55c7a5a3067
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2022-11-15 15:38:00 -0300

    Adding Pod CLTypingLabel - Title Label Main Screen

--- ChatApp/Controllers/WelcomeViewController.swift
+++ ChatApp/Controllers/WelcomeViewController.swift
@@ -6,26 +6,28 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
     
     
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+        titleLabel.text = "⚡️FlashChat"
         
-        titleLabel.text = ""
-        var characterIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            characterIndex += 1
-        }
+//        titleLabel.text = ""
+//        var characterIndex = 0.0
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            characterIndex += 1
+//        }
         
         
     }
--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+platform :ios, '13.0'
 
 target 'ChatApp' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -7,6 +7,6 @@ target 'ChatApp' do
 
   # Pods for ChatApp
   
-  pod 'CLTypingLabel'
+  pod 'CLTypingLabel', '~> 0.4.0'
 
 end
