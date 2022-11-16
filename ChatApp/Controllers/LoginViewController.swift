78099c2a497edeae21f7189c5c818a9cb7e560e0
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2022-11-16 14:17:46 -0300

    User Auth with new flow to create an Account

--- ChatApp/Controllers/LoginViewController.swift
+++ ChatApp/Controllers/LoginViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
 
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var adviceLabel: UILabel!
     
-    
+    @IBAction func createAccount(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "DontHaveAccount", sender: self)
+    }
     
     @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    self.adviceLabel.font = UIFont.preferredFont(forTextStyle: .body)
+                    self.adviceLabel.text = e.localizedDescription
+                    print(e.localizedDescription) // Localized para mostrar o erro no Idioma do Dispositivo
+                } else {
+                    // If not get error
+                    // Navigate to the ChatViewController
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
 }
