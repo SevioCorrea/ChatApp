321f4af5670a6d6744b0e6a8549bbccba1999616
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2022-11-22 13:29:26 -0300

    Order and limit data with Cloud Firestore

--- ChatApp/Controllers/ChatViewController.swift
+++ ChatApp/Controllers/ChatViewController.swift
@@ -38,9 +38,12 @@ class ChatViewController: UIViewController {
     // Load messages
     func loadMessages() {
         
-
-        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+        
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
             self.messages = []
+                
             if let e = error {
                 print("There was an issue retrieving data from Firestore. \(e)")
             } else {
@@ -67,7 +70,7 @@ class ChatViewController: UIViewController {
         
         // Acessando usuário que está logado
         if let messageBody = messageTextField.text, let messageSender = Auth.auth().currentUser?.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { (error) in
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
                 if let e = error {
                     print("There was an issue saving data to Firestore, \(e)")
                 } else {
